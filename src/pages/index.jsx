71bc526386254377d716af095cd4d4f73e90fcff
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-14 12:47:44 -0300

    update: index.jsx added first views

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,14 @@
 export default function Inicio(){
-    return 'Olá '
+    const a  = 1;
+    const b = 3;
+    console.log(a*b)
+    
+    return (
+        <div>
+            <h1>Fundamentos next-js</h1>
+            <h2>Vamos estudar o framework</h2>
+
+            
+        </div>
+    )
 }
