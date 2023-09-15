f7ad129c0e50fe87d7549c7cb73fbd1702b54592
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-15 09:32:49 -0300

    update: added props.titulo

--- src/pages/Estiloso.jsx
+++ src/pages/Estiloso.jsx
@@ -1,9 +1,9 @@
 import Layout from '../components/Layout'
 import styles from '../styles/Estiloso.module.css'
 
-const Estiloso = () => {
+const Estiloso = (props) => {
     return (
-        <Layout>
+        <Layout titulo={props.text}>
             <div className={styles.roxo}>
 
                 <h1>Estilo usando css modules</h1>
