8c82a65367c6c27f5cbe2d7db06bdabaa0d3eca7
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-15 08:57:11 -0300

    added: Exemplo.jsx

---
+++ src/pages/Exemplo.jsx
@@ -0,0 +1,18 @@
+export default function Exemplo(){
+    return(
+        <div>
+            <h1>Página exemplo</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec tristique lectus. Mauris interdum tortor at urna scelerisque varius. Nunc ac urna vitae tellus finibus sodales quis eu magna. Etiam dictum ac lectus a congue. Quisque porttitor mollis libero quis molestie. Donec ac varius est, ornare gravida sem. Vestibulum id sem eu dolor tincidunt tempus. Curabitur hendrerit, metus vitae luctus feugiat, dui ligula consectetur nisi, nec lacinia libero dolor sit amet justo. Duis quis lacus nibh. Donec felis mauris, bibendum at urna ac, pellentesque tristique tellus. Vestibulum tristique dui id rutrum scelerisque. Aenean ornare porta justo, quis tristique massa laoreet vel. Donec malesuada finibus auctor. Nullam ullamcorper tincidunt enim ut tempor. Etiam tempor augue et suscipit commodo. Ut in egestas lorem.
+
+Aenean feugiat eget risus ut rutrum. Morbi fringilla vitae nibh vitae dictum. Nam faucibus lacus quis tellus sagittis, quis varius orci aliquet. Suspendisse convallis tortor nec vulputate ornare. Fusce tincidunt faucibus lacinia. Sed velit nibh, efficitur in hendrerit non, tincidunt ac enim. Aenean at dolor eu turpis tristique rhoncus a nec risus. Duis in ligula nisi.
+
+In vitae dolor nisl. In hac habitasse platea dictumst. Sed tellus nunc, iaculis sed ultricies ac, tincidunt in metus. Ut ullamcorper auctor diam, id laoreet nibh tempus sed. Quisque facilisis est ex, ut dapibus metus tincidunt tempor. Donec eget iaculis sapien. Mauris interdum augue sit amet tortor tristique molestie. Suspendisse in dui in nulla pellentesque placerat in et est. Duis ac libero in turpis eleifend porttitor. Proin auctor ex ante, sed mollis est lobortis in. Integer rutrum, nisl non maximus maximus, eros magna iaculis urna, a condimentum ex nunc non orci. Sed at tortor tincidunt, tincidunt nisl vitae, vestibulum libero.
+
+Nam sit amet viverra enim. In scelerisque libero in sem porttitor, vel vulputate ipsum consequat. Mauris et sapien leo. Sed nec vestibulum ante. Duis mattis dolor neque, sed eleifend lacus porta et. Maecenas dictum neque vitae tellus consequat, id consectetur sem convallis. Pellentesque tincidunt commodo justo, a scelerisque velit sollicitudin vel. Quisque sit amet semper orci. Nullam sit amet placerat nunc. Nulla facilisi.
+
+Ut tincidunt eleifend justo, id consectetur nisl rhoncus sit amet. Donec auctor purus eu purus congue sodales. Cras consequat nulla ac velit accumsan elementum. Aenean ut sapien eu augue fermentum tempor. Nullam suscipit arcu vitae vestibulum tincidunt. Ut sapien nisl, sagittis vitae suscipit non, sollicitudin vel lacus. Integer tristique nec tellus ut rutrum. Praesent tincidunt ipsum et diam consectetur luctus. Suspendisse luctus nisi id egestas porttitor.
+
+</p>
+        </div>
+    )
+}
