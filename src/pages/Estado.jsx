e84b5a0677af9c0a036e5ece579b14f471793cfd
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-15 09:32:32 -0300

    added Estado.jsx

---
+++ src/pages/Estado.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Layout from "../components/Layout";
+
+export default function Estado() {
+
+    const [numero, setNumero] = useState(0);
+
+
+    function incrementar(){
+        
+        setNumero(numero + 1)
+
+    }
+    
+    return (
+        <Layout titulo="Componente com estado">
+
+            <span>{numero}</span>
+            <button onClick={() => incrementar()}>Incrementar</button>
+        </Layout>
+    )
+}
